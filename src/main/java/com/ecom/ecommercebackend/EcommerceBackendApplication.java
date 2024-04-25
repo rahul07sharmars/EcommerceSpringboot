f29708f1adee3afb48ad9bcea1be9fd462a65eb4
Rahul Sharma <rahul.sharma@aidash.com>
2024-04-25 15:28:41 +0530

    init project

---
+++ src/main/java/com/ecom/ecommercebackend/EcommerceBackendApplication.java
@@ -0,0 +1,14 @@
+package com.ecom.ecommercebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceBackendApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Running Test");
+		SpringApplication.run(EcommerceBackendApplication.class, args);
+	}
+
+}
